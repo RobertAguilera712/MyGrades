c7004ae090a2666b2ef30e11c63e82eb6ab1a21e
Robert Aguilera <rc7.aguilera@gmail.com>
2026-03-08 21:01:49 -0600

    Editar grupos

--- app/groups/routes.py
+++ app/groups/routes.py
@@ -10,7 +10,7 @@ from app.models import UserRole, Group, Person, User, Owner
 from .forms import GroupForm
 from flask_login import login_user, current_user, logout_user, login_required
 from jinja2_fragments.flask import render_block
-from .services import add_group
+from .services import add_group, edit_group
 
 groups = Blueprint(
     "groups",
@@ -33,6 +33,31 @@ def index():
     if "HX-Request" in request.headers:
         return render_block(template, "module", groups=groups)
     return render_template(template, groups=groups)
+
+@groups.route("/edit/<int:id>", methods=["GET", "POST"])
+@login_required
+@role_required([UserRole.OWNER])
+def edit_form(id):
+    form = GroupForm()
+    group = Group.query.get(id)
+    if form.save.data and form.validate_on_submit():
+        if edit_group(form, group):
+            flash("Grupo guardado con éxito", "success")
+            return redirect(url_for("groups.index"))
+        else:
+            flash("Ocurrió un erro al guardar el grupo", "error")
+    else:
+        print(form.errors)
+
+    template = "groups_form.jinja2"
+    title = "Editar Grupo"
+    form.name.data = group.name
+    if "HX-Request" in request.headers:
+        return render_block(
+            template, "module", title=title, form=form
+        )
+    return render_template(template, title=title, form=form)
+
 
 
 @groups.route("/add", methods=["GET", "POST"])
--- app/groups/services.py
+++ app/groups/services.py
@@ -2,6 +2,19 @@ from .forms import GroupForm
 from app.models import Group
 from app.extensions import db
 
+def edit_group(form: GroupForm, group: Group) -> bool:
+    try:
+        group.name = form.name.data
+        db.session.commit()
+        return True
+    except Exception as ex:
+        db.session.rollback()
+        ## TODO: ADD LOGGER
+        print(f"Error while editing group {str(ex)}")
+    return False
+
+
+
 def add_group(form: GroupForm, school_id: int) -> bool:
     try:
         group = Group(name = form.name.data, school_id=school_id)
